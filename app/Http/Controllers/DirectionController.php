675f49964e5fc3477ae8b422c6e2e9e770490c51
Luis <2123100609@soy.utj.edu.mx>
2024-11-30 15:20:19 -0600

    comentarios

--- app/Http/Controllers/DirectionController.php
+++ app/Http/Controllers/DirectionController.php
@@ -20,8 +20,8 @@ class DirectionController extends Controller
             'user_id' => 'required|integer|exists:users,id',
             'state' => 'required|max:100',
             'city' => 'required|max:100',
-            'name' => 'nullable|max:100',
-            'residence' => 'nullable|max:100',
+            'name' => 'required|max:100',
+            'residence' => 'required|max:100',
             'postal_code' => 'required|integer|max:99999999999',
             'description' => 'required|max:100',
         ]);
@@ -40,6 +40,7 @@ class DirectionController extends Controller
     {
         return $direction;
     }
+
 
     /**
      * Update the specified resource in storage.
